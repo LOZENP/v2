b7643adb1f4420d2f4a17acf41bd26ad1bdced4e
LOZENP <zeniaorcalero@gmail.com>
2026-05-23 18:48:50 +0800

    Add bit32 compatibility functions and clean up code

--- src/utils.lua
+++ src/utils.lua
@@ -1,5 +1,60 @@
 -- utils.lua
+
 local Utils = {}
+
+-- bit32 compat shim (works on LuaJIT, Lua5.1, Luau)
+local bit32 = bit32 or (function()
+    local b = {}
+
+    function b.bxor(a, x)
+        local r, m = 0, 1
+        while a > 0 or x > 0 do
+            if (a % 2) ~= (x % 2) then r = r + m end
+            a = math.floor(a / 2)
+            x = math.floor(x / 2)
+            m = m * 2
+        end
+        return r
+    end
+
+    function b.band(a, x)
+        local r, m = 0, 1
+        while a > 0 and x > 0 do
+            if (a % 2 == 1) and (x % 2 == 1) then r = r + m end
+            a = math.floor(a / 2)
+            x = math.floor(x / 2)
+            m = m * 2
+        end
+        return r
+    end
+
+    function b.bor(a, x)
+        local r, m = 0, 1
+        while a > 0 or x > 0 do
+            if (a % 2 == 1) or (x % 2 == 1) then r = r + m end
+            a = math.floor(a / 2)
+            x = math.floor(x / 2)
+            m = m * 2
+        end
+        return r
+    end
+
+    function b.bnot(a)
+        return b.bxor(a, 0xFFFFFFFF)
+    end
+
+    function b.rshift(a, n)
+        return math.floor(a / (2 ^ n))
+    end
+
+    function b.lshift(a, n)
+        return (a * (2 ^ n)) % (2 ^ 32)
+    end
+
+    return b
+end)()
+
+Utils.bit32 = bit32
 
 function Utils.randomSeed()
     math.randomseed(os.time())
@@ -8,7 +63,7 @@ end
 function Utils.randName(len)
     len = len or math.random(8, 14)
     local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    local name = chars:sub(math.random(27, 52), math.random(27, 52)) -- start uppercase
+    local name = chars:sub(math.random(27, 52), math.random(27, 52))
     for i = 1, len - 1 do
         local pos = math.random(1, #chars)
         name = name .. chars:sub(pos, pos)
@@ -35,13 +90,6 @@ end
 
 function Utils.xorBytes(a, b)
     return bit32.bxor(a, b)
-end
-
-function Utils.tableContains(tbl, val)
-    for _, v in ipairs(tbl) do
-        if v == val then return true end
-    end
-    return false
 end
 
 function Utils.serializeTable(tbl)
